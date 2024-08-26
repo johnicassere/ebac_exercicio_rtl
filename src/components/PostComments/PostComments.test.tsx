d1083b80d8b589b9e90418c39249fcd81d01564a
johnicassere <johnicassere@hotmail.com>
2024-08-26 13:46:34 -0300

    correção

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,35 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Post from '.';
-import PostComment from '.';
+import PostComment from './index';
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+describe('Teste componente PostComment', () => {
+    test('Test Componente', () => {
+        const { debug } = render(<PostComment/>)
+        debug()
+    })
+    it('Renderizar o componente corretamente', () => {
+       render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
-});
+    });  
+})
+
+describe('Teste componente PostComment', () => {
+    it('Deve adicionar dois comentários', () => {
+        render(<PostComment/>);
+        fireEvent.change(screen.getByTestId('campo-tex'), {
+            target: {
+                value: 'teste1 campo',
+            }
+        });
+        fireEvent.click(screen.getByTestId('campo-btn'));
+
+        fireEvent.change(screen.getByTestId('campo-tex'), {
+            target: {
+                value: 'teste2 campo',
+            }
+        })
+        fireEvent.click(screen.getByTestId('campo-btn'));
+
+        expect(screen.getAllByTestId('qtd-comentearios')).toHaveLength(2)
+})
+})
